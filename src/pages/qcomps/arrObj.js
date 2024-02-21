70b19bbd78f5dc3ed12dd14d1b26447db12fcf7d
viv-dan <81843793+viv-dan@users.noreply.github.com>
2024-02-21 16:27:39 -0500

    class activity 022124

--- src/pages/qcomps/arrObj.js
+++ src/pages/qcomps/arrObj.js
@@ -1,67 +1,63 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
-let nextId = 3;
+let nextId = 3
 const initialList = [
   { id: 0, title: 'Big Bellies', seen: false },
   { id: 1, title: 'Lunar Landscape', seen: false },
   { id: 2, title: 'Terracotta Army', seen: true },
-];
+]
 
 export default function BucketList() {
-  const [myList, setMyList] = useState(initialList);
+  const [myList, setMyList] = useState(initialList)
+
   const [yourList, setYourList] = useState(
-    initialList
-  );
+    initialList.map((item) => {
+      return { ...item }
+    })
+  )
 
   function handleToggleMyList(artworkId, nextSeen) {
-    const tmpList = myList.map(e => {
-        if (e.id === artworkId) {
-            e.seen = nextSeen
-        }
-        return e
-    });
-    setMyList(tmpList);
+    const tmpList = myList.map((e) => {
+      if (e.id === artworkId) {
+        e.seen = nextSeen
+      }
+      return e
+    })
+    setMyList(tmpList)
   }
 
   function handleToggleYourList(artworkId, nextSeen) {
-    const tmpList = yourList.map(e => {
-        if (e.id === artworkId) {
-            e.seen = nextSeen
-        }
-        return e
-    });
-    setYourList(tmpList);
+    const tmpList = yourList.map((e) => {
+      if (e.id === artworkId) {
+        e.seen = nextSeen
+      }
+      return e
+    })
+    setYourList(tmpList)
   }
 
   return (
     <>
       <h1>Art Bucket List</h1>
       <h2>My list of art to see:</h2>
-      <ItemList
-        artworks={myList}
-        onToggle={handleToggleMyList} />
+      <ItemList artworks={myList} onToggle={handleToggleMyList} />
       <h2>Your list of art to see:</h2>
-      <ItemList
-        artworks={yourList}
-        onToggle={handleToggleYourList} />
+      <ItemList artworks={yourList} onToggle={handleToggleYourList} />
     </>
-  );
+  )
 }
 
 function ItemList({ artworks, onToggle }) {
   return (
     <ul>
-      {artworks.map(artwork => (
+      {artworks.map((artwork) => (
         <li key={artwork.id}>
           <label>
             <input
-              type="checkbox"
+              type='checkbox'
               checked={artwork.seen}
-              onChange={e => {
-                onToggle(
-                  artwork.id,
-                  e.target.checked
-                );
+              onChange={(e) => {
+                onToggle(artwork.id, e.target.checked)
               }}
             />
             {artwork.title}
@@ -69,5 +65,5 @@ function ItemList({ artworks, onToggle }) {
         </li>
       ))}
     </ul>
-  );
+  )
 }
--- src/pages/qcomps/artistsRemoveArr.js
+++ src/pages/qcomps/artistsRemoveArr.js
@@ -1,31 +1,32 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 let initialArtists = [
   { id: 0, name: 'Marta Colvin Andrade' },
-  { id: 1, name: 'Lamidi Olonade Fakeye'},
-  { id: 2, name: 'Louise Nevelson'},
-];
+  { id: 1, name: 'Lamidi Olonade Fakeye' },
+  { id: 2, name: 'Louise Nevelson' },
+]
 
 export default function List() {
-  const [artists, setArtists] = useState(
-    initialArtists
-  );
+  const [artists, setArtists] = useState(initialArtists)
 
   return (
     <>
       <h1>Inspiring sculptors:</h1>
       <ul>
-        {artists.map(artist => (
+        {artists.map((artist) => (
           <li key={artist.id}>
             {artist.name}{' '}
-            <button onClick={() => {
-              artists.splice(artist.id, 1)
-            }}>
+            <button
+              onClick={() => {
+                console.log(artist.id)
+                setArtists(artists.slice(artist.id))
+              }}
+            >
               Delete
             </button>
           </li>
         ))}
       </ul>
     </>
-  );
+  )
 }
